c8ca50bc1c2e521a94f9eab33dc067f617b8897d
rsfaltas <163517133+rsfaltas@users.noreply.github.com>
2024-07-07 02:08:38 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,13 +1,13 @@
 def operation(item,shopping_list,operation):
-    if operation=='add':
+    if operation=='1':
         shopping_list.append(item)
         return "Item added"
-    if operation=='delete':
+    if operation=='2:
         if item in shopping_list:
             shopping_list.remove(item)
             return "Item deleted"
         else :return "Item does not exist in your list"
-    if operation=='review':
+    if operation=='3':
         return shopping_list
       
 def display_menu():  
@@ -26,16 +26,16 @@ def main():
         if choice == '1':
             # Prompt for and add an item
             item=input("Enter your item to add : ").lower()
-            print(operation(item,shopping_list,'add'))
+            print(operation(item,shopping_list,choice))
             pass
         elif choice == '2':
             # Prompt for and remove an item
             item=input("Enter your item ").lower()
-            print(operation(item,shopping_list,'delete'))
+            print(operation(item,shopping_list,choice))
             pass
         elif choice == '3':
             # Display the shopping list
-            print(operation(item,shopping_list,'review'))
+            print(operation(item,shopping_list,choice))
             pass
         elif choice == '4':
             print("Goodbye!")
