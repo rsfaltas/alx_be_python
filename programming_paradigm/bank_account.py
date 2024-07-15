057570b55ed3ae3a57298886b7c83a5a00a447a3
rsfaltas <163517133+rsfaltas@users.noreply.github.com>
2024-07-15 13:25:50 +0300

    Create bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+# bank_account.py
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self._account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self._account_balance += amount
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self._account_balance:
+            self._account_balance -= amount
+            return True
+        elif amount > self._account_balance:
+            return False
+        else:
+            print("Withdrawal amount must be positive.")
+            return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self._account_balance:.2f}")
